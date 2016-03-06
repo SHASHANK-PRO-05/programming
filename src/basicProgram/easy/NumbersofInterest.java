1419353a28c3f5f9bb4418730a35bfe25a1e935e
shashank kapoor <shashank-pro-05@gmail.com>
2016-03-06 13:21:57 +0530

    these were easy

---
+++ src/basicProgram/easy/NumbersofInterest.java
@@ -0,0 +1,99 @@
+package basicProgram.easy;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+
+public class NumbersofInterest {
+	static int numChar;
+	static int curChar;
+	static byte[] buffer = new byte[1024];
+	static InputStream stream;
+	static PrintWriter out;
+
+	public static void main(String[] args) throws InputMismatchException, IOException {
+		stream = System.in;
+		out = new PrintWriter(new BufferedOutputStream(System.out));
+		StringBuilder builder = new StringBuilder();
+
+		int t = readInt();
+		while (t-- != 0) {
+			int n = readInt(), x = readInt(), y = readInt();
+			long[] queue = new long[y + 1];
+			int sum = n * x;
+			for (int i = 0; i < n && i < y; i++) {
+				queue[i] = x;
+			}
+			if (y > n)
+				queue[n] = sum;
+			for (int i = n + 1; i < y; i++) {
+				queue[i] = queue[i - 1] * 2 - queue[i - n - 1];
+			}
+			builder.append(queue[y - 1] + "\n");
+		}
+		out.print(builder);
+		out.flush();
+		out.close();
+	}
+
+	public static int read() throws IOException {
+		if (numChar <= curChar) {
+			curChar = 0;
+			numChar = stream.read(buffer);
+			if (numChar <= 0) {
+				return -1;
+			}
+		}
+		return buffer[curChar++];
+	}
+
+	public static long readLong() throws IOException, InputMismatchException {
+		int c = read();
+		if (c == -1)
+			throw new IOException();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		boolean negative = false;
+		if (c == '-') {
+			negative = true;
+			c = read();
+		}
+		long res = 0;
+		while (!isSpaceChar(c)) {
+			if (c < '0' || c > '9')
+				throw new InputMismatchException();
+			res *= 10;
+			res += (c - '0');
+			c = read();
+		}
+		if (negative)
+			return -res;
+		return res;
+	}
+
+	public static int readInt() throws IOException, InputMismatchException {
+		return (int) readLong();
+	}
+
+	public static String readString() throws IOException {
+		int c = read();
+		if (c == -1)
+			throw new IOException();
+		while (isSpaceChar(c)) {
+			c = read();
+		}
+		StringBuilder builder = new StringBuilder();
+		while (!isSpaceChar(c)) {
+			builder.append((char) c);
+			c = read();
+		}
+		return builder.toString();
+	}
+
+	public static boolean isSpaceChar(int c) {
+		return c == ' ' || c == '\n' || c == '\t' || c == '\r' || c == -1;
+	}
+}
